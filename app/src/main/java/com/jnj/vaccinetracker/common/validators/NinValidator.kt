8afbf6dd781aba38e37d27e338b16b49f61620f3
sherrif10 <sharfspace@gmail.com>
2024-10-07 16:23:54 +0300

    remove commented lines

--- app/src/main/java/com/jnj/vaccinetracker/common/validators/NinValidator.kt
+++ app/src/main/java/com/jnj/vaccinetracker/common/validators/NinValidator.kt
@@ -5,9 +5,11 @@ import javax.inject.Inject
 class NinValidator @Inject constructor() {
 
     companion object {
+        /**
+         * The regex ^[A-Z0-9]{14}$ matches exactly 14 alphanumeric characters (letters or digits)
+         * with case insensitivity due to RegexOption.IGNORE_CASE 
+         */       
         private val NIN_REGEX = Regex("^[A-Z0-9]{14}$", RegexOption.IGNORE_CASE)
-
-        //  private val NIN_REGEX = Regex("^[a-zA-Z][MF]\\d{2}[a-zA-Z\\d]{7}[a-zA-Z]{3}$")
     }
 
     fun validate(valueToValidate: String): Boolean {
