16a99e64c502dc3b6bd53726fc46a6ee313d0a1b
sherrif10 <sharfspace@gmail.com>
2024-10-07 16:33:46 +0300

    Add comments on validations

--- app/src/main/java/com/jnj/vaccinetracker/common/validators/NinValidator.kt
+++ app/src/main/java/com/jnj/vaccinetracker/common/validators/NinValidator.kt
@@ -5,10 +5,11 @@ import javax.inject.Inject
 class NinValidator @Inject constructor() {
 
     companion object {
-        /**
+         /**
          * The regex ^[A-Z0-9]{14}$ matches exactly 14 alphanumeric characters (letters or digits)
          * with case insensitivity due to RegexOption.IGNORE_CASE 
-         */       
+         * This regex will now allow  both CM00000000FFKH and CM00000000FF6H, 
+         */        
         private val NIN_REGEX = Regex("^[A-Z0-9]{14}$", RegexOption.IGNORE_CASE)
     }
 
