90e5e3c625d46820ea629050decd1b7ac3f052d8
sherrif10 <sharfspace@gmail.com>
2024-10-07 15:51:48 +0300

    VRVU-184:Fix Nin validation regex

--- app/src/main/java/com/jnj/vaccinetracker/common/validators/NinValidator.kt
+++ app/src/main/java/com/jnj/vaccinetracker/common/validators/NinValidator.kt
@@ -5,7 +5,9 @@ import javax.inject.Inject
 class NinValidator @Inject constructor() {
 
     companion object {
-        private val NIN_REGEX = Regex("^[a-zA-Z][MF]\\d{2}[a-zA-Z\\d]{7}[a-zA-Z]{3}$")
+        private val NIN_REGEX = Regex("^[A-Z0-9]{14}$", RegexOption.IGNORE_CASE)
+
+        //  private val NIN_REGEX = Regex("^[a-zA-Z][MF]\\d{2}[a-zA-Z\\d]{7}[a-zA-Z]{3}$")
     }
 
     fun validate(valueToValidate: String): Boolean {
