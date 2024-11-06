7c4a3ac759ebfbe5cb8340932217f8387c1889f8
druchniewicz <druchniewicz@soldevelo.com>
2024-11-06 15:12:44 +0100

    VRVU-195: Handled downloading CSV file

--- app/src/main/java/com/jnj/vaccinetracker/visitsoverview/screens/VisitsListFragment.kt
+++ app/src/main/java/com/jnj/vaccinetracker/visitsoverview/screens/VisitsListFragment.kt
@@ -1,16 +1,22 @@
 package com.jnj.vaccinetracker.visitsoverview.screens
 
+import android.content.ContentValues
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.view.LayoutInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.jnj.vaccinetracker.R
+import com.jnj.vaccinetracker.common.data.database.typealiases.dateNow
 import com.jnj.vaccinetracker.common.data.models.Constants
 import com.jnj.vaccinetracker.common.ui.BaseFragment
 import com.jnj.vaccinetracker.common.util.DateUtil
@@ -26,7 +32,9 @@ import com.soywiz.klock.DateTime
 import com.soywiz.klock.jvm.toDate
 import java.util.Locale
 
-class VisitsListFragment(private val visitsKey: String) : BaseFragment(), VisitsOverviewDatePickerDialog.VisitsOverviewDatePickerListener {
+@RequiresApi(Build.VERSION_CODES.Q)
+class VisitsListFragment(private val visitsKey: String) : BaseFragment(),
+    VisitsOverviewDatePickerDialog.VisitsOverviewDatePickerListener {
 
     companion object {
         private const val START_DATE_PICKER_DIALOG_TAG = "startDatePicker"
@@ -39,13 +47,18 @@ class VisitsListFragment(private val visitsKey: String) : BaseFragment(), Visits
     private var selectedStartDate: DateTime? = null
     private var selectedEndDate: DateTime? = null
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_visits_list, container, false)
 
         setupRecyclerView()
         loadVisitsData()
         setupObservers()
         setupFiltersButton()
+        setupDownloadButtons()
 
         return binding.root
     }
@@ -101,25 +114,82 @@ class VisitsListFragment(private val visitsKey: String) : BaseFragment(), Visits
         }
     }
 
-    private fun applyFilters(visits: List<VisitDataDTO> = visitsListViewModel.visitDTOs.value ?: emptyList()) {
+    private fun setupDownloadButtons() {
+        binding.btnDownloadExcel.setOnClickListener {
+
+        }
+
+        binding.btnDownloadCsv.setOnClickListener {
+            exportToCSV(visitsAdapter.currentList)
+        }
+    }
+
+    private fun exportToCSV(visits: List<VisitDataDTO>) {
+        val resolver = requireContext().contentResolver
+        val fileName = "${buildFileName()}.csv"
+        val mimeType = "text/csv"
+        val titleRowColumns = "Visit Date, Client ID, Client name, Phone Number \n"
+        val contentValues = ContentValues().apply {
+            put(MediaStore.Downloads.DISPLAY_NAME, fileName)
+            put(MediaStore.Downloads.MIME_TYPE, mimeType)
+            put(MediaStore.Downloads.RELATIVE_PATH, Environment.DIRECTORY_DOWNLOADS)
+        }
+
+        val uri = resolver.insert(MediaStore.Downloads.EXTERNAL_CONTENT_URI, contentValues)
+        uri?.let {
+            resolver.openOutputStream(uri).use { outputStream ->
+                outputStream?.bufferedWriter()?.use { writer ->
+                    writer.write(titleRowColumns)
+                    visits.forEach { visit ->
+                        writer.write("${visit.formattedStartDateTime}, ${visit.participant.participantId}, ${visit.participant.fullName}, ${visit.participant.phone ?: ""} \n")
+                    }
+                }
+            }
+            Toast.makeText(requireContext(), "File saved in Downloads directory", Toast.LENGTH_LONG)
+                .show()
+        } ?: kotlin.run {
+            Toast.makeText(requireContext(), "Failed when saving file", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun buildFileName(): String {
+        return "${visitsKey.replace(" ", "_")}_${
+            DateUtil.convertDateToString(
+                dateNow(),
+                DateFormat.FORMAT_DATE.toString()
+            )
+        }"
+    }
+
+    private fun applyFilters(
+        visits: List<VisitDataDTO> = visitsListViewModel.visitDTOs.value ?: emptyList()
+    ) {
         val searchText = binding.searchBox.text.toString().lowercase(Locale.getDefault())
         val filteredVisits = visits.filter { visit ->
-            val dateMatches = (selectedStartDate == null || visit.startDatetime >= selectedStartDate?.toDate()) &&
-                    (selectedEndDate == null || visit.startDatetime <= selectedEndDate?.toDate())
-            val textSearchMatches = visit.participant.participantId.lowercase(Locale.getDefault()).contains(searchText)
-                    || visit.participant.fullName.lowercase(Locale.getDefault()).contains(searchText)
+            val dateMatches =
+                (selectedStartDate == null || visit.startDatetime >= selectedStartDate?.toDate()) &&
+                        (selectedEndDate == null || visit.startDatetime <= selectedEndDate?.toDate())
+            val textSearchMatches =
+                visit.participant.participantId.lowercase(Locale.getDefault()).contains(searchText)
+                        || visit.participant.fullName.lowercase(Locale.getDefault())
+                    .contains(searchText)
 
             dateMatches && textSearchMatches
         }
 
-        binding.totalVisitCount.text = getString(R.string.visits_overview_total_visit_count_label, filteredVisits.size)
+        binding.totalVisitCount.text =
+            getString(R.string.visits_overview_total_visit_count_label, filteredVisits.size)
 
         visitsAdapter.submitList(filteredVisits)
     }
 
     private fun showDatePickerDialog(isStartDate: Boolean) {
-        val datePickerDialog = VisitsOverviewDatePickerDialog(selectedDate = if (isStartDate) selectedStartDate else selectedEndDate)
-        datePickerDialog.show(childFragmentManager, if (isStartDate) START_DATE_PICKER_DIALOG_TAG else END_DATE_PICKER_DIALOG_TAG)
+        val datePickerDialog =
+            VisitsOverviewDatePickerDialog(selectedDate = if (isStartDate) selectedStartDate else selectedEndDate)
+        datePickerDialog.show(
+            childFragmentManager,
+            if (isStartDate) START_DATE_PICKER_DIALOG_TAG else END_DATE_PICKER_DIALOG_TAG
+        )
     }
 
     override fun onDatePicked(date: DateTime?, tag: String?) {
@@ -140,7 +210,12 @@ class VisitsListFragment(private val visitsKey: String) : BaseFragment(), Visits
     }
 
     private fun formatDate(date: DateTime?): String {
-        return date?.let { DateUtil.convertDateToString(date.toDate(), DateFormat.FORMAT_DATE.toString()) } ?: "Not set"
+        return date?.let {
+            DateUtil.convertDateToString(
+                date.toDate(),
+                DateFormat.FORMAT_DATE.toString()
+            )
+        } ?: "Not set"
     }
 
     private fun showVisitDetailsDialog(visitData: VisitDataDTO) {
