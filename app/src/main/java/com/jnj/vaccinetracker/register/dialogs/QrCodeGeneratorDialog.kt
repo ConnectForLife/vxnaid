02858603b1b65cd76b773af8b85c4b28b63166a9
sherrif10 <sharfspace@gmail.com>
2024-11-12 16:57:56 +0300

    add dependecies for qr code generation

--- app/src/main/java/com/jnj/vaccinetracker/register/dialogs/QrCodeGeneratorDialog.kt
+++ app/src/main/java/com/jnj/vaccinetracker/register/dialogs/QrCodeGeneratorDialog.kt
@@ -4,17 +4,40 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.fragment.app.DialogFragment
+import com.google.zxing.BarcodeFormat
 import com.jnj.vaccinetracker.R
+import com.journeyapps.barcodescanner.BarcodeEncoder
 
-class QrCodeGeneratorDialog : DialogFragment() {
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.dialog_qr_code_generator, container, false)
-    }
+class QrCodeGeneratorDialog : DialogFragment(R.layout.dialog_qr_code_generator) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val inputText = view.findViewById<EditText>(R.id.input_text)
+        val btnGenerateQrCode = view.findViewById<Button>(R.id.btnGenerateQrCode)
+        val imageViewQrCode = view.findViewById<ImageView>(R.id.imageView_qrCode)
+
+        btnGenerateQrCode.setOnClickListener {
+            val text = inputText.text.toString().trim()
+
+            if (text.isNotEmpty()) {
+                try {
+                    val barcodeEncoder = BarcodeEncoder()
+                    val bitmap = barcodeEncoder.encodeBitmap(
+                        text,
+                        BarcodeFormat.QR_CODE,
+                        250,
+                        250
+                    )
+                    imageViewQrCode.setImageBitmap(bitmap)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
     }
 }
