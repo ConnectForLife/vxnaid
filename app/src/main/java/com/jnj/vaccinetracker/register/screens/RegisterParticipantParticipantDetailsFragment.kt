b48010ae19652d2ad2ae6624bc1875ecbf913371
sherrif10 <sharfspace@gmail.com>
2024-11-20 16:14:05 +0300

    add validator for birthweight to only allow up to 9 kg

--- app/src/main/java/com/jnj/vaccinetracker/register/screens/RegisterParticipantParticipantDetailsFragment.kt
+++ app/src/main/java/com/jnj/vaccinetracker/register/screens/RegisterParticipantParticipantDetailsFragment.kt
@@ -215,14 +215,20 @@ class RegisterParticipantParticipantDetailsFragment : BaseFragment(),
             viewModel.setPhone(phoneNumber)
             flowViewModel.phoneNumber.set(phoneNumber)
         }
-        
+
         binding.editBirthWeight.doAfterTextChanged {
-            val birthWeight = it?.toString().orEmpty()
-            viewModel.setBirthWeight(birthWeight)
-            val currentDetails = flowViewModel.registerDetails.value
-            if (currentDetails != null) {
-                val updatedDetails = currentDetails.copy(birthWeight = birthWeight)
-                flowViewModel.registerDetails.set(updatedDetails)
+            val birthWeightString = it?.toString().orEmpty()
+            val birthWeight = birthWeightString.toIntOrNull()
+
+            if (birthWeight != null && birthWeight >= 9) {
+                binding.editBirthWeight.text = null
+            } else if (birthWeight != null) {
+                viewModel.setBirthWeight(birthWeight.toString())
+                val currentDetails = flowViewModel.registerDetails.value
+                if (currentDetails != null) {
+                    val updatedDetails = currentDetails.copy(birthWeight = birthWeight.toString())
+                    flowViewModel.registerDetails.set(updatedDetails)
+                }
             }
         }
 
