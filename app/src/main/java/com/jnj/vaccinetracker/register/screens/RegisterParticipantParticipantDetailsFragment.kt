7dbf284a950dde7a7555d6a877c3bad4340b1226
sherrif10 <sharfspace@gmail.com>
2024-11-21 12:42:09 +0300

    Add validator weight in Kg on registration page

--- app/src/main/java/com/jnj/vaccinetracker/register/screens/RegisterParticipantParticipantDetailsFragment.kt
+++ app/src/main/java/com/jnj/vaccinetracker/register/screens/RegisterParticipantParticipantDetailsFragment.kt
@@ -217,12 +217,18 @@ class RegisterParticipantParticipantDetailsFragment : BaseFragment(),
         }
         
         binding.editBirthWeight.doAfterTextChanged {
-            val birthWeight = it?.toString().orEmpty()
-            viewModel.setBirthWeight(birthWeight)
-            val currentDetails = flowViewModel.registerDetails.value
-            if (currentDetails != null) {
-                val updatedDetails = currentDetails.copy(birthWeight = birthWeight)
-                flowViewModel.registerDetails.set(updatedDetails)
+            val birthWeightString = it?.toString().orEmpty()
+            val birthWeight = birthWeightString.toIntOrNull()
+
+            if (birthWeight != null && birthWeight >= 9) {
+                binding.editBirthWeight.text = null
+            } else if (birthWeight != null) {
+                viewModel.setBirthWeight(birthWeight.toString())
+                val currentDetails = flowViewModel.registerDetails.value
+                if (currentDetails != null) {
+                    val updatedDetails = currentDetails.copy(birthWeight = birthWeight.toString())
+                    flowViewModel.registerDetails.set(updatedDetails)
+                }
             }
         }
 
