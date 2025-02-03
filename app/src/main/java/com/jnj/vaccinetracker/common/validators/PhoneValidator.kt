c7151b24a6998d64db1ae100a85df150982fee57
druchniewicz <druchniewicz@soldevelo.com>
2025-02-03 10:41:22 +0100

    Fixed phone number validation

--- app/src/main/java/com/jnj/vaccinetracker/common/validators/PhoneValidator.kt
+++ app/src/main/java/com/jnj/vaccinetracker/common/validators/PhoneValidator.kt
@@ -1,6 +1,5 @@
 package com.jnj.vaccinetracker.common.validators
 
-import com.jnj.vaccinetracker.common.helpers.logWarn
 import io.michaelrocks.libphonenumber.android.PhoneNumberUtil
 import javax.inject.Inject
 
@@ -10,21 +9,10 @@ class PhoneValidator @Inject constructor(private val phoneNumberUtil: PhoneNumbe
         /**
          * only numbers and white spaces
          */
-        private val phoneRegex = "^256[732]\\d{8}\$".toRegex()
+        private val phoneRegex = "^256\\d{9}\$".toRegex()
     }
 
     fun validate(fullPhoneNumber: String): Boolean {
-        if (!phoneRegex.matches(fullPhoneNumber)) {
-            logWarn("$fullPhoneNumber not matching with $phoneRegex")
-            return false
-        }
-        val phoneWithPlusPrefix = "+$fullPhoneNumber"
-        val phoneNumber = try {
-            phoneNumberUtil.parse(phoneWithPlusPrefix, null)
-        } catch (e: Exception) {
-            logWarn("Phone number invalid: ${e.message}")
-            return false
-        }
-        return phoneNumberUtil.isValidNumber(phoneNumber)
+        return phoneRegex.matches(fullPhoneNumber)
     }
 }
