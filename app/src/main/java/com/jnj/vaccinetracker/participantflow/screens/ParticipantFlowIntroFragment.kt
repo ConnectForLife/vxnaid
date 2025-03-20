d181164c5be0d0956c721a0f364986f67442c1e2
sherrif10 <sharfspace@gmail.com>
2025-03-20 11:00:05 +0300

    remove populateWorkflowSteps function, nolonger needed

--- app/src/main/java/com/jnj/vaccinetracker/participantflow/screens/ParticipantFlowIntroFragment.kt
+++ app/src/main/java/com/jnj/vaccinetracker/participantflow/screens/ParticipantFlowIntroFragment.kt
@@ -16,6 +16,13 @@ import com.jnj.vaccinetracker.databinding.FragmentParticipantFlowIntroBinding
 import com.jnj.vaccinetracker.databinding.ItemParticipantFlowItemBinding
 import com.jnj.vaccinetracker.participantflow.ParticipantFlowViewModel
 
+
+/**
+ * @author maartenvangiel
+ * @author tbuehler
+ * @author druelens
+ * @version 2
+ */
 class ParticipantFlowIntroFragment : BaseFragment() {
 
     private val viewModel: ParticipantFlowViewModel by activityViewModels { viewModelFactory }
@@ -39,72 +46,10 @@ class ParticipantFlowIntroFragment : BaseFragment() {
             navigateToFragment(ParticipantFlowParticipantIdFragment())
         }
 
-        // Add dynamic content
-//        populateWorkflowSteps(inflater)
-
         setHasOptionsMenu(true)
         (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         return binding.root
-    }
-
-    private fun populateWorkflowSteps(inflater: LayoutInflater) {
-        var index = 1
-
-        viewModel.workflowItems.forEach { step ->
-            val view = DataBindingUtil.inflate<ItemParticipantFlowItemBinding>(
-                inflater, R.layout.item_participant_flow_item, binding.authStepsContainer, true
-            )
-
-            when (step) {
-                ParticipantFlowViewModel.WorkflowItem.ID_CARD -> {
-                    view.label = this.getString(R.string.match_or_register_patient_step_id_card)
-                    view.stepIndex = this.getString(R.string.match_or_register_patient_step_index, index.toString())
-                    view.imgId.setImageResource(R.drawable.ic_id_card)
-                }
-                ParticipantFlowViewModel.WorkflowItem.PHONE -> {
-                    view.label = this.getString(R.string.match_or_register_patient_step_phone)
-                    view.stepIndex = this.getString(R.string.match_or_register_patient_step_index, index.toString())
-                    view.imgId.setImageResource(R.drawable.ic_baseline_phone)
-                }
-                ParticipantFlowViewModel.WorkflowItem.MOTHER_NAME -> {
-                    view.label = this.getString(R.string.participant_flow_mother_name_label)
-                    view.stepIndex = this.getString(R.string.match_or_register_patient_step_index, index.toString())
-                    view.imgId.setImageResource(R.drawable.ic_account)
-                }
-                ParticipantFlowViewModel.WorkflowItem.IRIS_SCAN -> {
-                    view.label = this.getString(R.string.match_or_register_patient_step_iris_scan)
-                    view.stepIndex = this.getString(R.string.match_or_register_patient_step_index, index.toString())
-                    view.imgId.setImageResource(R.drawable.ic_eyeglasses_right)
-                    view.imgId.updateLayoutParams {
-                        width = 153.toPx()
-                        height = 153.toPx()
-                    }
-                }
-                ParticipantFlowViewModel.WorkflowItem.MATCHING -> {
-                    view.label = this.getString(R.string.match_or_register_patient_step_identify)
-                    view.stepIndex = this.getString(R.string.match_or_register_patient_step_index, index.toString())
-                    view.imgId.updateLayoutParams {
-                        width = 120.toPx()
-                        height = 120.toPx()
-                    }
-                }
-                ParticipantFlowViewModel.WorkflowItem.VISIT -> {
-                    view.label = this.getString(R.string.match_or_register_patient_step_visit)
-                    view.stepIndex = this.getString(R.string.match_or_register_patient_step_index, index.toString())
-                    view.imgId.setImageResource(R.mipmap.ic_launcher_foreground)
-                    view.imgId.updateLayoutParams {
-                        width = 140.toPx()
-                        height = 140.toPx()
-                    }
-                }
-            }
-            index++
-
-            if (index <= viewModel.workflowItems.size) {
-                inflater.inflate(R.layout.item_participant_flow_divider, binding.authStepsContainer, true)
-            }
-        }
     }
 
     private fun navigateToFragment(fragment: Fragment) {
@@ -113,6 +58,5 @@ class ParticipantFlowIntroFragment : BaseFragment() {
             .addToBackStack(null)
             .commit()
     }
-
     private fun Int.toPx() = this * resources.displayMetrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT
 }
