c5a8d0d2b6db35bd4ebd4fe151f807dc8a912c8b
sokiror <okiror1vinald@gmail.com>
2025-04-15 16:45:23 +0300

    button

--- app/src/main/java/com/jnj/vaccinetracker/visit/dialog/VisitRegisteredSuccessDialog.kt
+++ app/src/main/java/com/jnj/vaccinetracker/visit/dialog/VisitRegisteredSuccessDialog.kt
@@ -46,26 +46,15 @@ import javax.inject.Inject
  * @author timonelen
  * @version 1
  */
-class VisitRegisteredSuccessDialog : BaseDialogFragment(),
-    ScheduleVisitDatePickerDialog.OnDateSelectedListener {
+class VisitRegisteredSuccessDialog : BaseDialogFragment(), ScheduleVisitDatePickerDialog.OnDateSelectedListener {
 
     companion object {
         private const val ARG_NEXT_VISIT = "next_visit"
         private const val PARTICIPANT = "participant"
         private const val CURRENT_VISIT_UUID = "currentVisitUuid"
 
-        fun create(
-            nextVisit: UpcomingVisit?,
-            participant: ParticipantSummaryUiModel?,
-            currentVisitUuid: String?
-        ): VisitRegisteredSuccessDialog {
-            return VisitRegisteredSuccessDialog().apply {
-                arguments = bundleOf(
-                    ARG_NEXT_VISIT to nextVisit,
-                    PARTICIPANT to participant,
-                    CURRENT_VISIT_UUID to currentVisitUuid
-                )
-            }
+        fun create(nextVisit: UpcomingVisit?, participant: ParticipantSummaryUiModel?, currentVisitUuid: String?): VisitRegisteredSuccessDialog {
+            return VisitRegisteredSuccessDialog().apply { arguments = bundleOf(ARG_NEXT_VISIT to nextVisit, PARTICIPANT to participant, CURRENT_VISIT_UUID to currentVisitUuid) }
         }
     }
 
@@ -81,22 +70,13 @@ class VisitRegisteredSuccessDialog : BaseDialogFragment(),
     private val nextVisit: UpcomingVisit? by lazy { requireArguments().getParcelable(ARG_NEXT_VISIT) }
     private var visitDate: DateTime? = null
     private var canFinish: Boolean = false
-    private val participant: ParticipantSummaryUiModel? by lazy {
-        requireArguments().getParcelable(
-            PARTICIPANT
-        )
-    }
+    private val participant: ParticipantSummaryUiModel? by lazy { requireArguments().getParcelable(PARTICIPANT) }
     private val currentVisitUuid: String? by lazy { requireArguments().getString(CURRENT_VISIT_UUID) }
-    @Inject
-    lateinit var createVisitUseCase: CreateVisitUseCase
-    @Inject
-    lateinit var userRepository: UserRepository
-    @Inject
-    lateinit var syncSettingsRepository: SyncSettingsRepository
-    @Inject
-    lateinit var configurationManager: ConfigurationManager
-    @Inject
-    lateinit var visitManager: VisitManager
+    @Inject lateinit var createVisitUseCase: CreateVisitUseCase
+    @Inject lateinit var userRepository: UserRepository
+    @Inject lateinit var syncSettingsRepository: SyncSettingsRepository
+    @Inject lateinit var configurationManager: ConfigurationManager
+    @Inject lateinit var visitManager: VisitManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -105,45 +85,29 @@ class VisitRegisteredSuccessDialog : BaseDialogFragment(),
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding = DataBindingUtil.inflate(
-            inflater,
-            R.layout.dialog_visit_registered_success,
-            container,
-            false
-        )
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.dialog_visit_registered_success, container, false)
         binding.nextVisit = nextVisit
         binding.executePendingBindings()
         visitDateTextView = binding.root.findViewById(R.id.next_visit_date_value)
         visitScheduleResultTextView = binding.root.findViewById(R.id.visit_schedule_result_label)
-        nextVisitDateContainerLinearLayout =
-            binding.root.findViewById(R.id.next_visit_date_container)
-        proposedDateContainerLinearLayout =
-            binding.root.findViewById(R.id.proposed_next_visit_date_container)
+        nextVisitDateContainerLinearLayout = binding.root.findViewById(R.id.next_visit_date_container)
+        proposedDateContainerLinearLayout = binding.root.findViewById(R.id.proposed_next_visit_date_container)
         saveVisitButton = binding.root.findViewById(R.id.btn_save_visit)
         closeButton = binding.root.findViewById(R.id.btn_finish)
         proposedDateTextVisit = binding.root.findViewById(R.id.proposed_next_visit_date_value)
-
+        
         closeButton.visibility = View.GONE
 
         lifecycleScope.launch {
             val nextVisitProposedDateAsLocalDate = findProposedNextVisitDateAsLocalDate()
             proposedDateTextVisit.text = nextVisitProposedDateAsLocalDate?.toString() ?: ""
 
-            val nextVisitProposedDateAsDate = Date.from(
-                nextVisitProposedDateAsLocalDate
-                    ?.atStartOfDay(ZoneId.of(Constants.UTC_TIME_ZONE_NAME))?.toInstant()
-            )
+            val nextVisitProposedDateAsDate = Date.from(nextVisitProposedDateAsLocalDate
+                ?.atStartOfDay(ZoneId.of(Constants.UTC_TIME_ZONE_NAME))?.toInstant())
             val proposedDateAsDate = nextVisitProposedDateAsDate?.let { DateTime.fromUnix(it.time) }
             binding.nextVisitDatePickerButton.setOnClickListener {
-                ScheduleVisitDatePickerDialog(
-                    proposedDateAsDate,
-                    this@VisitRegisteredSuccessDialog
-                ).show(childFragmentManager, "scheduleVisitDatePickerDialog")
+                ScheduleVisitDatePickerDialog(proposedDateAsDate, this@VisitRegisteredSuccessDialog).show(childFragmentManager, "scheduleVisitDatePickerDialog")
             }
 
             if (proposedDateAsDate != null) {
@@ -152,46 +116,28 @@ class VisitRegisteredSuccessDialog : BaseDialogFragment(),
         }
 
         binding.btnSaveVisit.setOnClickListener {
-            lifecycleScope.launch {
-                try {
-                    validateDate()
-                    if (visitDate != null) {
-                        createVisitUseCase.createVisit(
-                            buildNextVisitObject(
-                                participant!!,
-                                Date(visitDate!!.unixMillisLong)
-                            )
-                        )
-                        val visitDateAsText = visitDate!!.format(DateFormat.FORMAT_DATE)
-                        visitScheduleResultTextView.text =
-                            "${getString(R.string.visit_schedule_visit_saved_successfully_label)} $visitDateAsText"
-                        visitScheduleResultTextView.setTextColor(
-                            ContextCompat.getColor(
-                                requireContext(),
-                                R.color.successDark
-                            )
-                        )
-                        nextVisitDateContainerLinearLayout.visibility = View.GONE
-                        proposedDateContainerLinearLayout.visibility = View.GONE
-                        saveVisitButton.visibility = View.GONE
-                        closeButton.visibility = View.VISIBLE
-
-                        val layoutParams = closeButton.layoutParams as ConstraintLayout.LayoutParams
-                        layoutParams.horizontalBias = 0.5f
-                        closeButton.layoutParams = layoutParams
-                        canFinish = true
-                    }
-                } catch (ex: Exception) {
-                    visitScheduleResultTextView.text =
-                        getString(R.string.visit_schedule_visit_failed)
-                    visitScheduleResultTextView.setTextColor(
-                        ContextCompat.getColor(
-                            requireContext(),
-                            com.google.android.material.R.color.design_default_color_error
-                        )
-                    )
-                }
-            }
+           lifecycleScope.launch {
+               try {
+                   validateDate()
+                   if (visitDate != null) {
+                       createVisitUseCase.createVisit(buildNextVisitObject(participant!!, Date(visitDate!!.unixMillisLong)))
+                       val visitDateAsText = visitDate!!.format(DateFormat.FORMAT_DATE)
+                       visitScheduleResultTextView.text = "${getString(R.string.visit_schedule_visit_saved_successfully_label)} $visitDateAsText"
+                       visitScheduleResultTextView.setTextColor(ContextCompat.getColor(requireContext(), R.color.successDark))
+                       nextVisitDateContainerLinearLayout.visibility = View.GONE
+                       proposedDateContainerLinearLayout.visibility = View.GONE
+                       saveVisitButton.visibility = View.GONE
+                       closeButton.visibility = View.VISIBLE
+                       val layoutParams = closeButton.layoutParams as ConstraintLayout.LayoutParams
+                       layoutParams.horizontalBias = 0.5f
+                       closeButton.layoutParams = layoutParams
+                       canFinish = true
+                   }
+               } catch (ex: Exception) {
+                   visitScheduleResultTextView.text = getString(R.string.visit_schedule_visit_failed)
+                   visitScheduleResultTextView.setTextColor(ContextCompat.getColor(requireContext(), com.google.android.material.R.color.design_default_color_error))
+               }
+           }
         }
 
         binding.btnFinish.setOnClickListener {
@@ -215,10 +161,7 @@ class VisitRegisteredSuccessDialog : BaseDialogFragment(),
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
-    private suspend fun buildNextVisitObject(
-        participant: ParticipantSummaryUiModel,
-        visitDate: Date
-    ): CreateVisit {
+    private suspend fun buildNextVisitObject(participant: ParticipantSummaryUiModel, visitDate: Date): CreateVisit {
         val operatorUuid = userRepository.getUser()?.uuid
             ?: throw OperatorUuidNotAvailableException("Operator UUID not available")
         val locationUuid = syncSettingsRepository.getSiteUuid()
@@ -241,19 +184,14 @@ class VisitRegisteredSuccessDialog : BaseDialogFragment(),
     private suspend fun findVisitTypeOfNextVisit(): String {
         val participantBirthDate = participant!!.birthDateText
         val participantVisits = visitManager.getVisitsForParticipant(participant!!.participantUuid)
-        val currentVisitType = SubstancesDataUtil.getVisitTypeForCurrentVisit(
-            participantBirthDate,
-            participantVisits,
-            configurationManager
-        )
+        val currentVisitType = SubstancesDataUtil.getVisitTypeForCurrentVisit(participantBirthDate, participantVisits, configurationManager)
         val substancesConfig = configurationManager.getSubstancesConfig()
         val currentVaccine = substancesConfig.find { it.visitType == currentVisitType }
         if (currentVaccine == null) {
             return ""
         }
-        val nextVaccine =
-            substancesConfig.filter { it.weeksAfterBirth > currentVaccine.weeksAfterBirth }
-                .minByOrNull { it.weeksAfterBirth }
+        val nextVaccine = substancesConfig.filter { it.weeksAfterBirth > currentVaccine.weeksAfterBirth }
+            .minByOrNull { it.weeksAfterBirth }
         if (nextVaccine == null) {
             return ""
         }
@@ -263,8 +201,7 @@ class VisitRegisteredSuccessDialog : BaseDialogFragment(),
 
     private fun validateDate() {
         if (visitDate == null) {
-            val dateValidationText =
-                getString(R.string.dialog_missing_substances_empty_date_validation_message)
+            val dateValidationText = getString(R.string.dialog_missing_substances_empty_date_validation_message)
             binding.nextVisitDateValue.error = dateValidationText
             val hintTextColor = ContextCompat.getColor(requireContext(), R.color.errorLight)
             binding.nextVisitDateValue.setHintTextColor(hintTextColor)
