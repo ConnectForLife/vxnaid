671661d47ef99e5057a26673404c8f7f81ebf34e
Hillary Kaluuma <hkaluuma@idi.co.ug>
2025-05-06 18:54:24 +0300

    Added visit_type to draftvisits

--- app/src/main/java/com/jnj/vaccinetracker/visitsoverview/dto/VisitDataDTO.kt
+++ app/src/main/java/com/jnj/vaccinetracker/visitsoverview/dto/VisitDataDTO.kt
@@ -16,6 +16,26 @@ data class VisitDataDTO(
 ) {
     val formattedStartDateTime: String get() = DateUtil.convertDateToString(startDatetime, DateFormat.FORMAT_DATE.toString())
     val visitype: String
-        get() = attributes["Visit type Vxnaid"] ?: ""
-
+        get() {
+            val visitTypeVxnaid = attributes["Visit type Vxnaid"]
+            if (!visitTypeVxnaid.isNullOrEmpty()) {
+                return visitTypeVxnaid
+            } else {
+                val doseNumber = attributes["Dose number"] ?: attributes["Dose Number"] ?: ""
+                return when (doseNumber) {
+                    "1" -> "At Birth"
+                    "2" -> "6 weeks"
+                    "3" -> "10 weeks"
+                    "4" -> "14 weeks"
+                    "5" -> "6 months"
+                    "6" -> "7 months"
+                    "7" -> "8 months"
+                    "8" -> "9 months"
+                    "9" -> "1 year"
+                    "10" -> "18 months"
+                    "11" -> "2 years"
+                    else -> "Unknown"
+                }
+            }
+        }
 }
