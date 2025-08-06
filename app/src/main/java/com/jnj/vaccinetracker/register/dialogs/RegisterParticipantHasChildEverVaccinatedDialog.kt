e18dd19928af94e4f5b2e9643a77f657deeba2c9
[vinald] <okiror1vinald@gmail.com>
2025-08-06 09:05:10 +0300

    Reminder pop up to follow dates on the card

--- app/src/main/java/com/jnj/vaccinetracker/register/dialogs/RegisterParticipantHasChildEverVaccinatedDialog.kt
+++ app/src/main/java/com/jnj/vaccinetracker/register/dialogs/RegisterParticipantHasChildEverVaccinatedDialog.kt
@@ -23,14 +23,25 @@ class RegisterParticipantHasChildEverVaccinatedDialog : BaseDialogFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.dialog_register_participant_has_child_ever_vaccinated, container, false)
         binding.btnYes.setOnClickListener {
-            findParent<RegisterParticipationIsChildNewbornListener>()?.continueRegistrationWithCaptureVaccinesPage()
-            dismissAllowingStateLoss()
+            showFollowDatesInfoDialog()
         }
         binding.btnNo.setOnClickListener {
             findParent<RegisterParticipationIsChildNewbornListener>()?.continueRegistrationWithSuccessDialog()
             dismissAllowingStateLoss()
         }
         return binding.root
+    }
+
+    private fun showFollowDatesInfoDialog() {
+        val infoDialog = PastVisitDateInfoDialog.newInstance(
+            getString(R.string.medical_worker_follow_dates_title),
+            getString(R.string.medical_worker_follow_dates_message),
+            getString(R.string.continue_button_label)
+        ) {
+            findParent<RegisterParticipationIsChildNewbornListener>()?.continueRegistrationWithCaptureVaccinesPage()
+            dismissAllowingStateLoss()
+        }
+        infoDialog.show(parentFragmentManager, "infoDialog")
     }
 
     interface RegisterParticipationIsChildNewbornListener {
