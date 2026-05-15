b8efe080d5b7561c7557984f77f2acbc6a2a32e6
[vinald] <okiror1vinald@gmail.com>
2026-05-15 05:47:40 +0300

    shortened the names of services

--- app/src/main/java/com/jnj/vaccinetracker/common/data/models/ChildHealthPlusModels.kt
+++ app/src/main/java/com/jnj/vaccinetracker/common/data/models/ChildHealthPlusModels.kt
@@ -12,12 +12,12 @@ enum class ChildHealthPlusService(
     val serviceKey: String,
     val conceptName: String,
 ) {
-    VITAMIN_A("Vitamin A (Vit A)", "VIT_A", "Vitamin A Vxnaid"),
+    VITAMIN_A("Vit A", "VIT_A", "Vitamin A Vxnaid"),
     DEWORMING("Deworming", "DEWORMING", "Deworming Vxnaid"),
     MR2("MR2", "MR2", "Measles Rubella 2 (MR2) Vxnaid"),
-    HEPATITIS_B("Hepatitis B (HepB)", "HEPB", "HepB Vxnaid"),
-    HPV("Human Papillomavirus (HPV)", "HPV", "HPV Vxnaid"),
-    TETANUS("Tetanus (Td)", "TD", "Td Vxnaid");
+    HEPATITIS_B("HepB", "HEPB", "HepB Vxnaid"),
+    HPV("HPV", "HPV", "HPV Vxnaid"),
+    TETANUS("Td", "TD", "Td Vxnaid");
 }
 
 data class SelectedService(
