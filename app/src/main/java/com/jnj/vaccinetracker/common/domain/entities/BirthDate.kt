3a422e36fabbaaa81833561251ee46009c5f9ea9
[vinald] <okiror1vinald@gmail.com>
2026-05-19 04:14:28 +0300

    date fix, wrong vaccines fix, visit type fix

--- app/src/main/java/com/jnj/vaccinetracker/common/domain/entities/BirthDate.kt
+++ app/src/main/java/com/jnj/vaccinetracker/common/domain/entities/BirthDate.kt
@@ -2,6 +2,7 @@ package com.jnj.vaccinetracker.common.domain.entities
 
 import com.soywiz.klock.DateFormat
 import com.soywiz.klock.DateTime
+import java.util.Calendar
 
 data class BirthDate(val time: Long) {
     val year: Int
@@ -9,10 +10,14 @@ data class BirthDate(val time: Long) {
     val day: Int
 
     init {
-        val birthDateTime = DateTime(time)
-        year = birthDateTime.yearInt
-        month = birthDateTime.month.index1
-        day = birthDateTime.dayOfMonth
+        // Use device local timezone so that dates returned from OpenMRS (which stores dates in
+        // the facility's local timezone) are interpreted correctly. klock DateTime(time) is UTC-only
+        // and would shift the day back by the UTC offset (e.g. UTC+3 causes -1 day).
+        val cal = Calendar.getInstance()
+        cal.timeInMillis = time
+        year = cal.get(Calendar.YEAR)
+        month = cal.get(Calendar.MONTH) + 1
+        day = cal.get(Calendar.DAY_OF_MONTH)
     }
 
     fun toDateTime(): DateTime {
